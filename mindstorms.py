a7fa6bd31fec930dabaaac0cbd51ac7c0c5c5566
Samuel Havard <SamuelHavard@yahoo.com>
2016-12-09 11:32:03 -0500

    update

--- mindstorms.py
+++ mindstorms.py
@@ -7,9 +7,9 @@ window.bgcolor("red")
 def draw_square(angle):
     brad = turtle.Turtle()
     brad.right(angle * 10)
-    for x in range(1, 4):
-        brad.forward(100)
-        brad.right(90)
+    for x in range(0, 4):
+        brad.fd(100)
+        brad.rt(90)
 
 
 def draw_circle():
@@ -20,8 +20,8 @@ def draw_circle():
 def draw_triangle():
     bert = turtle.Turtle()
     for x in xrange(2):
-        bert.right(90)
-        bert.forward(100)
+        bert.rt(90)
+        bert.fd(100)
     bert.home()
 
 for x in range(0, 36):
